dddf61b1e44f0720e109f65bc124938095bbd8aa
Motaz Yusuf <50301755+motazyusuf@users.noreply.github.com>
2024-10-05 19:24:55 +0300

    Revert "Onboard view"

--- lib/core/failure/failure.dart
+++
@@ -1,6 +0,0 @@
-class Failure {
-  String statusCode;
-  String errorMessage;
-
-  Failure({required this.statusCode, required this.errorMessage});
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:med_app/med_app.dart';
+import 'package:med_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MedApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
