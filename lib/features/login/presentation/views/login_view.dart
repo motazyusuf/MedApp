07f6d14dbe3d9a0b8ba0a65f0237f95647e31e36
Motaz <motazyusuf@gmail.com>
2024-10-17 17:17:59 +0300

    Snack bar showed

--- lib/features/login/presentation/views/login_view.dart
+++ lib/features/login/presentation/views/login_view.dart
@@ -25,124 +25,166 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
+    var cubit = context.read<LogInCubit>();
     return Scaffold(
-      body: BlocBuilder<LogInCubit, LogInState>(
-        builder: (context, state) {
-          if (state is LogInLoading) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: ColorPalette.mainBlue,
-              ),
-            );
-          }
-          return SafeArea(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 70.h),
-                child: Form(
-                  key: formKey,
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 70.h),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                // Top page text
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 10.w),
                   child: Column(
-                    children: [
-                      // Top page text
-                      Padding(
-                        padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Welcome back",
-                                style: MyTextStyles.fontInter24MainBlueBold,
-                              ),
-                              verticalSpace(10),
-                              Text(
-                                "We're excited to have you back, can't wait to\nsee what you've "
-                                "been up to since you last\nlogged in.",
-                                style: MyTextStyles.fontInter14DarkGreyRegular,
-                              ),
-                              verticalSpace(30),
-                            ]),
-                      ),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome back",
+                          style: MyTextStyles.fontInter24MainBlueBold,
+                        ),
+                        verticalSpace(10),
+                        Text(
+                          "We're excited to have you back, can't wait to\nsee what you've "
+                          "been up to since you last\nlogged in.",
+                          style: MyTextStyles.fontInter14DarkGreyRegular,
+                        ),
+                        verticalSpace(30),
+                      ]),
+                ),
 
-                      // Email
-                      MyTextFormField(
-                          controller: emailController, hintText: "Email"),
-                      verticalSpace(20),
-                      // Password
-                      MyTextFormField(
-                        controller: passwordController,
-                        hintText: "Password",
-                        isObsecure: isObsecure,
-                        suffixIcon: GestureDetector(
-                          onTap: () {
+                // Email
+                    MyTextFormField(
+                        controller: emailController, hintText: "Email"),
+                    verticalSpace(20),
+                    // Password
+                    MyTextFormField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      isObsecure: isObsecure,
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isObsecure = !isObsecure;
+                          });
+                        },
+                        child: isObsecure
+                            ? const Icon(Icons.visibility)
+                            : const Icon(Icons.visibility_off),
+                      ),
+                    ),
+                    verticalSpace(20),
+
+                // Checkbox && Forgot password
+                Row(
+                  children: [
+                    Expanded(
+                      child: ListTileTheme(
+                        horizontalTitleGap: 0,
+                        child: CheckboxListTile(
+                          side: BorderSide(color: ColorPalette.darkGrey),
+                          contentPadding: EdgeInsets.zero,
+                          controlAffinity: ListTileControlAffinity.leading,
+                          title: Text(
+                            "Remember me",
+                            style: MyTextStyles.fontInter13DarkGreyRegular,
+                          ),
+                          value: isChecked,
+                          onChanged: (newBool) {
                             setState(() {
-                              isObsecure = !isObsecure;
+                              isChecked = newBool!;
                             });
                           },
-                          child: isObsecure
-                              ? const Icon(Icons.visibility)
-                              : const Icon(Icons.visibility_off),
                         ),
                       ),
-                      verticalSpace(20),
+                    ),
+                    Text(
+                      "Forgot password?",
+                      style: MyTextStyles.fontInter13MainBlueRegular,
+                    ),
+                  ],
+                ),
+                verticalSpace(10),
 
-                      // Checkbox && Forgot password
-                      Row(
-                        children: [
-                          Expanded(
-                            child: ListTileTheme(
-                              horizontalTitleGap: 0,
-                              child: CheckboxListTile(
-                                side: BorderSide(color: ColorPalette.darkGrey),
-                                contentPadding: EdgeInsets.zero,
-                                controlAffinity:
-                                    ListTileControlAffinity.leading,
-                                title: Text(
-                                  "Remember me",
-                                  style:
-                                      MyTextStyles.fontInter13DarkGreyRegular,
-                                ),
-                                value: isChecked,
-                                onChanged: (newBool) {
-                                  setState(() {
-                                    isChecked = newBool!;
-                                  });
-                                },
+                // Login button
+                    BlocListener<LogInCubit, LogInState>(
+                      listener: (context, state) {
+                        if (state is LogInLoading) {
+                          showDialog(
+                              barrierColor: Colors.transparent,
+                              context: context,
+                              builder: (context) {
+                                return Center(
+                                  child: CircularProgressIndicator(
+                                    backgroundColor: ColorPalette.darkBlue,
+                                    color: ColorPalette.mainBlue,
+                                  ),
+                                );
+                              });
+                        }
+                        if (state is LogInSuccess) {
+                          Navigator.of(context).pop();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Center(
+                              child: Text(
+                                "Logged in",
+                                style: MyTextStyles.fontInter24MainBlueBold,
                               ),
                             ),
-                          ),
-                          Text(
-                            "Forgot password?",
-                            style: MyTextStyles.fontInter13MainBlueRegular,
-                          ),
-                        ],
-                      ),
-                      verticalSpace(10),
-
-                      // Login button
-                      TextButton(
+                          ));
+                        }
+                        if (state is LogInFails) {
+                          Navigator.of(context).pop();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Center(
+                              child: Text(
+                                "Failed to log in!",
+                                style: MyTextStyles.fontInter24MainBlueBold
+                                    .copyWith(color: Colors.red),
+                              ),
+                            ),
+                          ));
+                        }
+                      },
+                      child: TextButton(
                           onPressed: () {
-                            var result = context.read<LogInCubit>().logIn(
+                            cubit.logIn(
                                 emailController.text, passwordController.text);
-                            print("Token is ${result.toString()}");
+                            FocusManager.instance.primaryFocus?.unfocus();
                           },
                           child: Text(
                             "Login",
                             style: MyTextStyles.fontInter16WhiteSemiBold,
                           )),
+                    ),
 
-                      verticalSpace(15),
-                      TermsAndConditionText(),
+                verticalSpace(15),
+                TermsAndConditionText(),
 
-                      verticalSpace(20),
-                      SignUpText(),
-                    ],
-                  ),
+                verticalSpace(20),
+                    SignUpText(),
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        )
+      // BlocBuilder<LogInCubit, LogInState>(
+      //   builder: (context, state) {
+      //     if (state is LogInLoading) {
+      //       return Center(
+      //         child: CircularProgressIndicator(
+      //           color: ColorPalette.mainBlue,
+      //         ),
+      //       );
+      //     }
+      //     if(state is LogInSuccess)
+      //       print("Token is ${state.token}");
+      //     return
+      //   },
+      // ),
     );
   }
 }
